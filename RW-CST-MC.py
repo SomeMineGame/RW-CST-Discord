f30a5f65ed9ebb978fa850588e8a46bb7d4010bb
SomeMineGame <someminegame@someminegame.com>
2025-10-30 14:59:13 -0500

    Simple Extras
    Added coinflip, magic8ball, rock paper scissors, and rock paper scissors lizard spock

--- RW-CST-MC.py
+++ RW-CST-MC.py
@@ -1,4 +1,4 @@
-import discord, typing, requests, os, asyncrcon
+import discord, typing, random, requests, os, asyncrcon
 from discord import app_commands
 from asyncrcon import AsyncRCON
 from discord import app_commands
@@ -104,5 +104,49 @@ async def update_command_tree(i: di):
     await i.response.send_message("Updated the command tree.", ephemeral=True)
 
 # MISC
+@bot.command(description="Flips a coim")
+@commands.cooldown(1, 30, commands.cooldowns.BucketType.member)
+async def coinflip(i:di):
+    side = random.randint(1, 2)
+    if side == 1:
+        await i.response.send_message("You got heads!")
+    else:
+        await i.response.send_message("You got tails!")
+        
+@bot.command(description="Get the answer to a yes or no question")
+async def magic8ball(i: di, message:str):
+    responses = ['Maybe so, maybe not', 'I think so!', "I think not!", "Oh no! Not ***THAT** question!", 'Lol! What kind of question was that?', "Shut up! I'm tired.", "Joke's on you, it won't happen.", "Always", "Never", "Yeah", "IDK, YOU TELL *ME*", "No way!", 'Totally bro', 'Sorry but, no.', 'Absolutely', 'I hate my job because of that question', 'Repeat and try again in simpler terms', 'Error, response not clear.', 'You are a dimwit for asking that.', 'Why not', 'Sure', 'Uhhhh, no?', 'Uhhhh, yes?', 'How **DARE** you ask the beast of magic!', "Yes", "No", "Certainly", "That is uncertain", "It can happen", 'It can happen... NOT', 'Probably', 'Probably not', 'Repeat', "Goodbye, that response was stupid", '0% Chance', "25% Chance", '50% Chance', '75% Chance', '100% Chance', 'Absolutely not', 'You think ***I*** know the answer to that?']
+    response = random.choice(responses)
+    await i.response.send_message(response)
+    
+@bot.command(description="Rock Paper Scissors against the bot")
+async def rps(i: di, choice: typing.Literal['Rock', 'Paper', 'Scissors']):
+    botchoices = ["Rock", "Paper", "Scissors"]
+    botoption = random.choice(botchoices)
+    outcomes = ["Rock Scissors", "Paper Rock", "Scissors Paper"]
+    if choice == botoption:
+        await i.response.send_message(f"You both chose {choice}. Nobody won.")
+    elif f"{botoption} {choice}" in outcomes:
+        await i.response.send_message(f"You chose {choice} and the bot chose {botoption}. You lose.")
+    elif f"{choice} {botoption}" in outcomes:
+        await i.response.send_message(f"You chose {choice} and the bot chose {botoption}. You win!")
+    else:
+        await i.response.send_message(f'Developer error debug:\nPlayer={choice} Bot={botoption}')
+      
+@bot.command(description="Rock Paper Scissors Lizard Spock against the bot")
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@commands.has_any_role("Sub $10")
+async def rpsls(i: di, choice: typing.Literal["Rock", "Paper", "Scissors", "Lizard", "Spock"]):
+    botoptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+    botoption = random.choice(botoptions)
+    outcomes = ["Rock Scissors", "Rock Lizard", "Paper Rock", "Paper Spock", "Scissors Paper", "Scissors Lizard", "Lizard Paper", "Lizard Spock", "Spock Rock", "Spock Scissors"]
+    if choice == botoption:
+        await i.response.send_message(f"You both chose {choice}. Nobody won.")
+    elif f"{botoption} {choice}" in outcomes:
+        await i.response.send_message(f"You chose {choice} and the bot chose {botoption}. You lose.")
+    elif f"{choice} {botoption}" in outcomes:
+        await i.response.send_message(f"You chose {choice} and the bot chose {botoption}. You win!")
+    else:
+        await i.response.send_message(f'Developer error debug:\nPlayer={choice} Bot={botoption}')
     
 client.run(bt.token)
